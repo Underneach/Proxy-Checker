73370710c891addf6485afc240b6590c13a079c1
Негр <137613889+Underneach@users.noreply.github.com>
2023-07-31 15:09:54 +0300

    Add files via upload

---
+++ build_nuitka.bat
@@ -0,0 +1,1 @@
+nuitka --onefile --show-progress --show-modules --remove-output --follow-import-to=os --follow-import-to=PySide6 --follow-import-to=httpx --include-plugin-directory=modules --include-data-file=modules\icon.ico=modules\icon.ico --include-data-file=modules\app.ui=modules\app.ui --windows-icon-from-ico=modules\icon.ico --enable-plugin=pyside6 --disable-console main.py
