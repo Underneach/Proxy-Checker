ae4e25ed09cf3b065f847dc5ef368af0bc925f3d
Негр <137613889+Underneach@users.noreply.github.com>
2023-07-31 12:10:23 +0000

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-pyinstaller --clean --onefile --noconsole --noconfirm --add-data "modules/app.ui;modules" --add-data "modules/icon.ico;modules" --icon "modules/icon.ico" main.py
-
-move "dist\main.exe" "main.exe"
-
-rd /s /q "dist"
-rd /s /q "build"
-del main.spec
